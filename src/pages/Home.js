0ac6ae7df291e45f0ac9f8f0bac67f4ea823a31b
hoyeon <hoyun293@naver.com>
2020-05-03 20:43:34 +0900

    #test : Google Login 테스트를 위한 임시 커밋

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Layout from "./Layout";
+import { GoogleLogin } from "react-google-login";
+
+const responseGoogle = (response) => {
+  console.log(response);
+};
+
 const Home = () => {
   return (
     <Layout>
@@ -10,6 +16,15 @@ const Home = () => {
       </p>
       <p>
         <Link to="/calculator">적금계산기 페이지</Link>
+      </p>
+      <p>
+        <GoogleLogin
+          clientId="80071714659-q1uctf5epkc1h1b7b107vmhvklhe4pgp.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
       </p>
     </Layout>
   );
