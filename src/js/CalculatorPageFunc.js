3ab4607f3ecceed53bf6a8036f5a956f5bce9bea
hoyeon <hoyun293@naver.com>
2020-05-05 22:29:07 +0900

    #feature: default formatter 미사용 및 prettier 사용으로 변경

--- src/js/CalculatorPageFunc.js
+++ src/js/CalculatorPageFunc.js
@@ -1,29 +1,27 @@
 const addTax2InterestObject = (interestObject, taxRate) => {
-
   const { interest, total } = interestObject;
   const tax = Math.round(interest * taxRate);
 
   interestObject.tax = tax;
   interestObject.total = total - tax;
   return interestObject;
-}
+};
 
 const calculateTax = (interestObject, taxType) => {
-
   const NORMAL_TAX = 0.154; // 일반과세
   const BREAK_TAX = 0.095; // 세금우대
   const MUTUAL_FINANCE = 0.014; // 상호금융권 및 새마을금고, 신협 저과세
 
-  if (taxType == "") {
+  if (taxType == '') {
     return addTax2InterestObject(interestObject, 0);
-  } else if (taxType == "1") {
+  } else if (taxType == '1') {
     return addTax2InterestObject(interestObject, NORMAL_TAX);
-  } else if (taxType == "2") {
+  } else if (taxType == '2') {
     return addTax2InterestObject(interestObject, BREAK_TAX);
-  } else if (taxType == "3") {
+  } else if (taxType == '3') {
     return addTax2InterestObject(interestObject, MUTUAL_FINANCE);
   }
-}
+};
 
 const compoundInterestCalculate = ({ won, month, rate, taxType }) => {
   let total = 0;
@@ -36,7 +34,7 @@ const compoundInterestCalculate = ({ won, month, rate, taxType }) => {
     principal: won * month,
     interest,
     total: Math.round(total),
-  }
+  };
 
   return calculateTax(interestObject, taxType);
 };
@@ -46,16 +44,15 @@ const simpleInterestCalculate = ({ won, month, rate, taxType }) => {
   const interestObject = {
     principal: won * month,
     interest,
-    total: Math.round(won * month + interest)
-  }
+    total: Math.round(won * month + interest),
+  };
   return calculateTax(interestObject, taxType);
 };
 
 export const getPrincipalInterestTotal = (status, wonMonthRateTaxtype) => {
+  console.log(wonMonthRateTaxtype);
 
-  console.log(wonMonthRateTaxtype)
-
-  if (status == "1") {
+  if (status == '1') {
     const compoundInterest = compoundInterestCalculate(wonMonthRateTaxtype);
     return compoundInterest;
   } else {
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,27 +1,27 @@
 // react-hot-loader를 사용하기 위한 import
-import { hot } from "react-hot-loader/root";
-import React from "react";
-import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
-import importedComponent from "react-imported-component";
-import Home from "./Home";
-import Loading from "../components/Loading";
+import { hot } from 'react-hot-loader/root';
+import React from 'react';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import importedComponent from 'react-imported-component';
+import Home from './Home';
+import Loading from '../components/Loading';
 //import DynamicPage from "./DynamicPage";
 //import NoMatch from "./NoMatch";
 
 const AsyncDynamicPage = importedComponent(
-  () => import(/* webpackChunkName: "Dynamic" */ "./DynamicPage"),
+  () => import(/* webpackChunkName: "Dynamic" */ './DynamicPage'),
   {
     LoadingComponent: Loading,
   }
 );
 const AsyncCalculatorPage = importedComponent(
-  () => import(/* webpackChunkName: "Calculator" */ "./Calculator"),
+  () => import(/* webpackChunkName: "Calculator" */ './Calculator'),
   {
     LoadingComponent: Loading,
   }
 );
 const AsyncNoMatch = importedComponent(
-  () => import(/* webpackChunkName: "NoMatch" */ "./NoMatch"),
+  () => import(/* webpackChunkName: "NoMatch" */ './NoMatch'),
   {
     LoadingComponent: Loading,
   }
