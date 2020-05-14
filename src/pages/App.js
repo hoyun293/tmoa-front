ebb00494b2083f5a5be887613c5f4c87fd7842af
Sambalim <limsungho07@hanmail.net>
2020-05-14 22:55:44 +0900

    Revert "feature: 개인정보약관 페이지 업로드"
    This reverts commit 8249d9527a1ccc6bcc21c45bcbf2c4d7fcb36747.

--- src/pages/App.js
+++ src/pages/App.js
@@ -4,7 +4,6 @@ import React from 'react';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import importedComponent from 'react-imported-component';
 import Home from './Home';
-import Privacy from './Privacy';
 import Loading from '../components/Loading';
 
 const AsyncDynamicPage = importedComponent(
@@ -33,7 +32,6 @@ const App = () => {
           <Route exact path="/" component={Home} />
           <Route exact path="/dynamic" component={AsyncDynamicPage} />
           <Route exact path="/calculator" component={AsyncCalculatorPage} />
-          <Route exact path="/privacy" component={Privacy} />
           <Route exact component={AsyncNoMatch} />
         </Switch>
       </div>
