b5a68a1314ae5adcc747b7d0c49e2cf5b7709659
hoyeon <hoyun293@naver.com>
2020-05-29 00:14:13 +0900

    #featrue: CSS작업 (1. GoalListComponent 초안작업)

---
+++ src/components/GoalListComponents/GoalListComponent.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+const GoalListCard = styled.div`
+    width: 100%;
+    margin-left: 2rem;
+    margin-right: 2rem;
+    height: 5rem;
+    display: flex;
+`;
+const GoalSummaryPart = styled.div`
+    flex: 3;
+`;
+const GoalImagePart = styled.div`
+    flext: 2;
+`;
+
+const GoalTitle = styled.div`
+`;
+const Goal
+const GoalListComponent = () =>{
+    return 
+}
+
+export default GoalListComponent;
