8bd39e35ca41e5a2cc2fb755aaf72c37a609abd1
Sambalim <limsungho07@hanmail.net>
2020-05-31 17:42:00 +0900

    test: 소셜로그인

--- src/pages/goalSearch/searchGoal.js
+++ src/pages/goalSearch/searchGoal.js
@@ -19,6 +19,26 @@ const searchGoal = () => {
     console.log(`${searchWord} 검색!`);
   }
 
+  const styleTest = {
+    display: 'block',
+    height: '22px',
+    lineHeight: '20px',
+    fontWeight: 300,
+    fontSize: '12px',
+    color: 'rgb(151, 161, 167)',
+    overflowWrap: 'break-word',
+    margin: '0px 4px 4px 0px',
+    padding: '0px 10px',
+    textDecoration: 'none',
+    background: 'rgb(255, 255, 255)',
+    borderWidth: '1px',
+    borderStyle: 'solid',
+    borderColor: 'rgb(207, 211, 214)',
+    borderImage: 'initial',
+    borderRadius: '10px',
+    transition: 'all 0.2s ease 0s'
+  }
+
   return (
     <Layout>
       <div style={{position: 'absolute', top: 0, width:'100%', height: 260, backgroundColor: '#118A59'}}></div>
@@ -42,7 +62,19 @@ const searchGoal = () => {
                   </Col>
                 </Row>
                 <Row>
-                  
+                  <Col span={24}>
+                    <ul style={{display:'flex', flexWrap: 'wrap', listStyle: 'none', padding: 0}}>
+                      <li>
+                        <a style={styleTest} href="/searchGoal">#테스트</a>
+                      </li>
+                      <li>
+                        <a style={styleTest} href="/searchGoal">#일이삼사오육칠팔</a>
+                      </li>
+                      <li>
+                        <a style={styleTest} href="/searchGoal">#일이삼사오육칠팔</a>
+                      </li>
+                    </ul>
+                  </Col>
                 </Row>
               </Card>
             </Col>
