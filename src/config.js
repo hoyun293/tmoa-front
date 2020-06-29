f0531c542269359ccea57efec4ad8ab29c994c9a
hoyeon <hoyun293@naver.com>
2020-06-29 15:01:14 +0900

    #test: 서버 URL 변경

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // 개발과 운영으로 나눠야 하지만 먼저 운영만
 // 민감한 내용은 추후에 소스에서 제거
 const config = {
-  BASE_URL: 'http://ec2-54-180-116-158.ap-northeast-2.compute.amazonaws.com:3000',
+  BASE_URL: 'https://tmoa-ts.site.',
   GOOGLE_CLIENT_ID: '510961742149-vnfho6456nlts5odenbbthgfhfl2ghnf.apps.googleusercontent.com',
   FACEBOOK_APP_ID: '528237391387780',
   REDIRECT_URL: 'https://sharp-murdock-7b2086.netlify.app',
